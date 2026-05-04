8aab69f4334817ee9df60a3c35fc83d3dc43fe4a
BrendaVieira21 <brendinhavieirinha2102@gmail.com>
2026-05-03 21:32:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/nome-repositorio/" : "/",
+  base: mode === "production" ? "/traveler-s-genesis-guide/" : "/",
   server: {
     host: "::",
     port: 8080,
